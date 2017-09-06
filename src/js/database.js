99ecf05c7e234fb23a69722bfd349486d55176bb
Basavaraj G <basu@Basavarajs-MacBook-Pro.local>
2017-09-06 19:41:39 -0400

    database class

--- src/js/database.js
+++ src/js/database.js
@@ -16,7 +16,7 @@ module.exports = class Database {
         this.connection = mysql.createConnection(conf);
     }
     query(sql, args) {
-        return new Promise( ( resolve, reject ) => {
+        return new Promise((resolve, reject) => {
             this.connection.query(sql, args, (err, rows) => {
                 if (err)
                     return reject( err );
@@ -26,7 +26,7 @@ module.exports = class Database {
     }
     close() {
         return new Promise((resolve, reject) => {
-            this.connection.end( err => {
+            this.connection.end(err => {
                 if (err)
                     return reject(err);
                 resolve();
